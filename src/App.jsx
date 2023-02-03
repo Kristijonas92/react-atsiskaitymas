e9d7456c9759cb48c05bfbc79fbccd16c1a5a6ae
Kristijonas92 <kristijonas.politika@gmail.com>
2023-02-03 13:21:19 +0200

    Added routes and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
+import Add from "./components/Add";
+
 
 const App = () => {
   return (
     <>
+      <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/add" element={<Add />} />
+      </Routes>
     </>
   );
 }
